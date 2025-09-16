885423e9ada2cd8a081cdd1c1a7389e7e13cadef
Martin Schweinberger <44954005+MartinSchweinberger@users.noreply.github.com>
2025-09-16 20:12:23 +1000

    skewness update

--- docs/libs/tabwid-1.1.3/tabwid.js
+++ docs/libs/tabwid-1.1.3/tabwid.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         fantome.appendChild(tabwid_link.cloneNode(true));
       }
   });
-<<<<<<< HEAD
 
   const shadowHosts = document.querySelectorAll('.flextable-shadow-host:not(:has(div > table.no-shadow-dom))');
   shadowHosts.forEach(host => {
@@ -30,7 +29,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
 });
-=======
-});
-
->>>>>>> c3420f19b30c0d3ab653cfff1be5b528ebf634e0
